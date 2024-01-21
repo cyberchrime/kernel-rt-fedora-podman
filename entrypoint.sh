2d339c2767f0a4d94c69c41b8ca0b35f01943456
Chris H. Meyer <christian.h.meyer@t-online.de>
2024-01-21 16:11:22 +0100

    Fix error

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,13 @@ sed -i "s/# define buildid .local/%define buildid .rt/g" kernel.spec
 wget -qO- https://cdn.kernel.org/pub/linux/kernel/projects/rt/6.6/patch-6.6.12-rt20.patch.xz | xz -d > patch-6.6.12-rt20.patch
 echo "Patch2: patch-6.6.12-rt20.patch" >> kernel.spec
 
-git clone https://src.fedoraproject.org/rpms/kernel.git
+# Fix for error
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git checkout -b 6.6.12-rt
+git add kernel.spec patch-6.6.12-rt20.patch
+git commit -m "Tmp commit to fix error"
+git branch -u origin/f39
 
 fedpkg local
 
